37f768ccd89c11bdeefdcbc34215e4e5756288a0
Matthew Cross <mattyc246@gmail.com>
2019-09-20 17:12:37 +0800

    game working with logic to stop game when you lose

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ let leftBoundary = 20;
 let rightBoundary = 330;
 let crossPoint = 125;
 let cloudCount = 1;
+let play;
+let timer;
+let smokeOp = 0;
+let gameSpeed = 1200;
+
+function startPlaying() {
+  play = setInterval(() => {
+    playRound();
+  }, 800);
+}
+
+function startTimer() {
+  timer = setInterval(() => {
+    time = time + 1;
+    $("#timer").html(`Time: ${time}`);
+  }, 1000);
+}
 
 function startPoint() {
   let min = 20;
@@ -19,6 +36,9 @@ function startPoint() {
 
 function playRound() {
   cloudCount++;
+  if (time % 15 == 0) {
+    gameSpeed += 200;
+  }
   $("#life-bar").after(
     `<img id="cloud-${cloudCount}" class="haze-cloud" src="./images/haze-cartoon.png">`
   );
@@ -26,7 +46,7 @@ function playRound() {
   $(`#cloud-${cloudCount}`).animate(
     { bottom: "20px" },
     {
-      duration: 1600,
+      duration: gameSpeed,
       step: function() {
         let cloudPosTop = Math.floor(
           $(this)
@@ -40,6 +60,29 @@ function playRound() {
           (cloudPosTop <= crossPoint + 20) | (cloudPosTop <= crossPoint) &&
           (leftBoundary <= cloudPosLeft && rightBoundary >= cloudPosLeft)
         ) {
+          $(this).stop();
+          $(this).animate(
+            { top: "20px" },
+            {
+              duration: 1600,
+              complete: function() {
+                $(this).remove();
+              }
+            }
+          );
+        }
+      },
+      complete: function() {
+        lives--;
+        if (lives == 0) {
+          clearInterval(play);
+          clearTimeout(timer);
+        } else {
+          $(`#api-${currentAPI}`).css("opacity", 1);
+          smokeOp = smokeOp + 0.15;
+          $(`#smokeImage`).css("opacity", smokeOp);
+          $(`#smokeImage-2`).css("opacity", smokeOp);
+          currentAPI++;
           $(this).remove();
         }
       }
@@ -59,13 +102,10 @@ $(document).ready(() => {
             rightBoundary = parseInt(e.target.style.left.split("p")[0]) + 50;
           }
         });
-        let timer = setInterval(() => {
-          time = time + 1;
-          $("#timer").html(`Time: ${time}`);
-        }, 1000);
-        let play = setInterval(() => {
-          playRound();
-        }, 800);
+        // Call Timer
+        startTimer();
+        // Call Play
+        startPlaying();
       });
     });
   });
